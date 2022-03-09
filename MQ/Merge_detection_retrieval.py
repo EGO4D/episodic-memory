db9f604862ea4d847e59db94277fcc5d45b2e11e
yushuo718 <yushuo718@163.com>
2022-03-09 20:46:34 +0800

    Modify submission generation file

--- MQ/Merge_detection_retrieval.py
+++ MQ/Merge_detection_retrieval.py
@@ -1,8 +1,7 @@
 import json
 
 result_det = 'output/detections_postNMS.json'
-result_rev = 'output/retreival_postNMS.json'
-
+result_rev = 'output/retrieval_postNMS.json'
 
 with open(result_det, 'r') as fobj:
     data_det = json.load(fobj)
@@ -10,9 +9,10 @@ with open(result_det, 'r') as fobj:
 with open(result_rev, 'r') as fobj:
     data_rev = json.load(fobj)
 
-data_submission = data_det
-data_submission['detection'] = data_submission.pop('results')
-data_submission['retrieval'] = data_rev['results']
+data_submission = {"version": "1.0", "challenge": "ego4d_moment_queries"}
+
+data_submission['detect_results'] = data_det['results']
+data_submission['retrieve_results'] = data_rev['results']
 
 with open("output/submission.json", "w") as fp:
     json.dump(data_submission,fp)
