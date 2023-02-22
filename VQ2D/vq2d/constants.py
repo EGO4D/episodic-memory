ee39732bfeea62c7665dc0281a7061a8e1b94d6e
Santhosh <sramakrishnan@utexas.edu>
2023-02-21 21:22:01 -0600

    changed image-format to jpg for speed+storage

--- VQ2D/vq2d/constants.py
+++ VQ2D/vq2d/constants.py
@@ -1,4 +1,4 @@
-IMAGE_NAME_PATTERN = "{}/frame_{:07d}.png"
+IMAGE_NAME_PATTERN = "{}/frame_{:07d}.jpg"
 CLIP_NAME_PATTERN = "{}.mp4"
 BBOX_NAME_PATTERN = "{}/{:07d}.npz"
 DATASET_FILE_TEMPLATE = "{}_annot.json.gz"
