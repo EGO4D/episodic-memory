819da1f68c933cccaccd93562698f447bc29dc99
vincentcartillier <vcartillier3@gatech.edu>
2023-02-23 15:40:48 -0500

    adding 90deg rotation to GT centroids to match the get transformation

--- VQ3D/VQ3D/API/metrics.py
+++ VQ3D/VQ3D/API/metrics.py
@@ -15,10 +15,21 @@ class angularError():
         angle = np.arccos(np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2)))
         return angle
 
+
+Rz_90 = np.array([[np.cos(-np.pi/2), -np.sin(-np.pi/2), 0, 0],
+                  [np.sin(-np.pi/2),  np.cos(-np.pi/2), 0, 0],
+                  [0, 0, 1, 0],
+                  [0, 0, 0, 1],
+                 ])
+
 class accuracy():
     def compute(self, t:np.ndarray, box1:BoundingBox, box2:BoundingBox) -> bool:
 
         c = (box1.center + box2.center) / 2.0
+        
+        c = np.append(c, 1.)
+        c = np.matmul(Rz_90, c)
+        c = c[:3] / c[3]
 
         d = np.linalg.norm(c-t)
         d_gt = np.linalg.norm(box1.center - box2.center)
