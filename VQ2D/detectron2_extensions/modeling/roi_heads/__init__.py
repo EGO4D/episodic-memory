3a495e82dc37b4da13994dbe9bd82d6ddd1da56f
Santhosh <sramakrishnan@utexas.edu>
2023-02-26 21:59:00 -0600

    add negative frames matter model

--- VQ2D/detectron2_extensions/modeling/roi_heads/__init__.py
+++ VQ2D/detectron2_extensions/modeling/roi_heads/__init__.py
@@ -1,6 +1,9 @@
+from .set_heads import (
+    SetTransROIHeads,
+)
 from .siam_heads import (
     SiameseROIHeads,
 )
 
 
-__all__ = ["SiameseROIHeads"]
+__all__ = ["SiameseROIHeads", "SetTransROIHeads"]
